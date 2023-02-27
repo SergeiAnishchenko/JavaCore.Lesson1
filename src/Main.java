b458d83f34876ecdc52eec77390f924324e83e2a
SergeiAnishchenko <79313219184@yandex.ru>
2023-02-27 21:05:55 +0300

    Домашка по JavaCore.Урок №8. Корректировка.

--- src/Main.java
+++ src/Main.java
@@ -147,6 +147,18 @@ public class Main {
             System.out.println(iteratorDrivers.next());
         }
 
+
+
+        Map<Transport, List<Mechanic>> mapTransportMechanic = new HashMap<>();
+
+        mapTransportMechanic.put(newCar,mechanics);
+        mapTransportMechanic.put(newTruck,mechanics);
+        mapTransportMechanic.put(newBus,mechanics);
+
+        for (Map.Entry<Transport,List<Mechanic>> entry:mapTransportMechanic.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
     }
 
     private static void printInfo(Transport<?> transport) {
